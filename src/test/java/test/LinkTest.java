4ee296cce6cd1b9a375b0c0acf5483094292b9bf
SvetlanaArkhincheeva <arhincheeva@gmail.com>
2018-12-11 11:19:20 +0700

    Initial commit

---
+++ src/test/java/test/LinkTest.java
@@ -0,0 +1,62 @@
+package test;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by s.arkhincheeva on 10.12.2018.
+ */
+public class LinkTest {
+    private static WebDriver driver;
+
+    @BeforeClass
+    public static void setup() {
+        System.setProperty("webdriver.chrome.driver", "C:/Users/s.arkhincheeva/chromedriver.exe");
+        driver = new ChromeDriver();
+        //driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+    }
+    @Test
+    public void testLink() throws InterruptedException {
+        driver.get("http://blog.csssr.ru/qa-engineer/");
+        driver.findElement(By.linkText("НАХОДИТЬ НЕСОВЕРШЕНСТВА")).click();
+        /*WebElement link = driver.findElement(By.linkText("Софт для быстрого создания скриншотов"));
+        Assert.assertEquals(link.getAttribute("href"), "http://app.prntscr.com/ru/");*/
+
+
+        driver.findElement(By.linkText("Софт для быстрого создания скриншотов")).click();
+        //driver
+       // driver.switchTo().window(driver.getWindowHandles().get(1));
+       Thread.sleep(3000);
+        ArrayList<String> tabs2 = new ArrayList<String> (driver.getWindowHandles());
+        driver.switchTo().window(tabs2.get(1));
+        Assert.assertEquals("Url page isn't correct", "https://app.prntscr.com/ru/", driver.getCurrentUrl());
+
+        /*WebElement loginField = driver.findElement(By.id("login"));
+        loginField.sendKeys("autotestorgua");
+        WebElement passwordField = driver.findElement(By.id("password"));
+        passwordField.sendKeys("testpass");
+        WebElement loginButton = driver.findElement(By.xpath("//button[text()='Войти']"));
+        loginButton.click();
+        WebElement profileUser = driver.findElement(By.cssSelector(".login-button__user"));
+        String mailUser = profileUser.getText();
+        Assert.assertEquals("autotestorgua@ukr.net", mailUser);*/
+    }
+    @AfterClass
+    public static void tearDown() {
+       /*WebElement menuUser = driver.findElement(By.cssSelector(".login-button__menu-icon"));
+        menuUser.click();
+        WebElement logoutButton = driver.findElement(By.id("login__logout"));
+        logoutButton.click();*/
+        driver.quit();
+    }
+}
